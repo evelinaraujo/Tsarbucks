aad37e6172224ea3116fbb0fd3da3af3da1137d5
evelyn araujo <evelynaraujo@evelyns-MacBook-Pro.local>
2017-11-16 21:52:18 -0800

    Updated database name

--- connection.php
+++ connection.php
@@ -1,12 +1,10 @@
-// change database name
-
 <?php
 $user = 'root';
 $password = 'root';
-$db = 'books'; 
+$db = 'tsarbucks'; 
 $host = 'localhost';
 $port = 3306;
 
 $connection = mysqli_connect("localhost", "root", "root" );
-$db = mysqli_select_db($connection, "books");
+$db = mysqli_select_db($connection, "tsarbucks");
 ?>
